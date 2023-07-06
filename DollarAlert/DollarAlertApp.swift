a845f414b378fc7fa54d99d30e74cc27e46c10bb
hernan.casla@gmail.com <Hernan>
2023-07-06 10:35:45 +0200

    Initial Commit

---
+++ DollarAlert/DollarAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DollarAlertApp.swift
+//  DollarAlert
+//
+//  Created by Osvaldo Ceballos on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DollarAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
