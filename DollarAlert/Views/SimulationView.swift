ce7e4a7efde7c0385d35cb74bff3b25b2b68df4b
hernan.casla@gmail.com <Hernan>
2023-10-13 17:32:06 +0200

    casi casi

---
+++ DollarAlert/Views/SimulationView.swift
@@ -0,0 +1,18 @@
+//
+//  SimulationView.swift
+//  DollarAlert
+//
+//  Created by Osvaldo Ceballos on 12/10/2023.
+//
+
+import SwiftUI
+
+struct SimulationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimulationView()
+}
---
+++ DollarAlert/Views/ToolBarView.swift
@@ -0,0 +1,18 @@
+//
+//  ToolBarView.swift
+//  DollarAlert
+//
+//  Created by Osvaldo Ceballos on 12/10/2023.
+//
+
+import SwiftUI
+
+struct ToolBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToolBarView()
+}
